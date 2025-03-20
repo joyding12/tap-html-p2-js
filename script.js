c1c7a01cf9a51fe8a248dc197f81f0b5db3d4de9
joyding12 <147291233+joyding12@users.noreply.github.com>
2025-03-20 18:32:18 +0000

    mar 20

--- script.js
+++ script.js
@@ -7,9 +7,30 @@ petdata = {
     "5" : { ill : "2%", costOfTreatment: 500 },
 }
 
+
 /*
 Hints
 document.getElementById("id").value;
 document.getElementById("id").addEventListener("click", function);
 document.addEventListener("DOMContentLoaded", function() {  });
 */
+
+function jcp() {
+    console.log("Button Clicked");
+    gender = document.getElementById("genderdd").value;
+    breed = document.getElementById("breedd").value;
+    ageYrs = document.getElementById("agedd").value;
+
+    avgTreatmentCost = petdata[ageYrs]["costOfTreatment"];
+    illnessChance = parseFloat(petdata[ageYrs]["ill"])/100;
+
+    ev= (avgTreatmentCost * illnessChance) * (1+gender) * (1+breed);
+    ev = ev.toFixed(2);
+
+    console.log(`Premium is: ${ev}`)
+    document.getElementById("premcal").innerHTML= "$" +ev;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("calcbtn").addEventListener("click",jcp);
+});
